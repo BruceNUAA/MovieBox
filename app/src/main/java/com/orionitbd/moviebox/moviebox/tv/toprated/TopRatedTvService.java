2e194eb585a5aa9f2a14191553a858aaa3ab1dca
Ezzaz <37590867+Ezzaz@users.noreply.github.com>
2018-05-05 02:14:42 +0600

    Service class added

--- app/src/main/java/com/orionitbd/moviebox/moviebox/tv/toprated/TopRatedTvService.java
+++ app/src/main/java/com/orionitbd/moviebox/moviebox/tv/toprated/TopRatedTvService.java
@@ -1,4 +1,15 @@
 package com.orionitbd.moviebox.moviebox.tv.toprated;
 
+import com.orionitbd.moviebox.moviebox.movie.upcoming.UpcomingMovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface TopRatedTvService {
+
+    @GET("movie/upcoming?")
+    Call<UpcomingMovieResponse> getUpcomingMovie( @Query("api_key")String API_KEY,
+                                                  @Query("language") String lang,
+                                                  @Query("page") String page);
 }

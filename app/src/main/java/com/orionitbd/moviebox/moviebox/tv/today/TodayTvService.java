9ce64aa0f649a861470e74808207cf32e00b5aee
Ezzaz <37590867+Ezzaz@users.noreply.github.com>
2018-05-10 23:37:49 +0600

    TV done

--- app/src/main/java/com/orionitbd/moviebox/moviebox/tv/today/TodayTvService.java
+++ app/src/main/java/com/orionitbd/moviebox/moviebox/tv/today/TodayTvService.java
@@ -1,4 +1,15 @@
 package com.orionitbd.moviebox.moviebox.tv.today;
 
+import com.orionitbd.moviebox.moviebox.tv.popular.PopularTvResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface TodayTvService {
+
+    @GET("tv/airing_today?")
+    Call<TodayTvResponse> getTodayResponse( @Query("api_key")String API_KEY,
+                                            @Query("language") String lang,
+                                            @Query("page") String page);
 }
